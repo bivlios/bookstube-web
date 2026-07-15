bff7d2169b902d4fd2ceaea5c460e10021e757c9
Igal Libedinsky <igal@bivlios.com>
2026-07-15 12:16:51 +0300

    style changes

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,8 @@ export default function Header({ lang, t }) {
     <header className="site-header">
       <a href={`/${lang}`} className="brand">
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/images/bookstube-logo.png" alt={t('ui.booksgiant')} className="brand-logo" />
-        <span className="brand-name">{t('ui.booksgiant')}</span>
+        <img src="/images/logo-sml.png" alt={t('ui.booksgiant')} className="brand-logo" />
+          {/*<span className="brand-name">{t('ui.booksgiant')}</span>*/}
       </a>
       <HeaderSearch lang={lang} placeholder={t('tagLibrary.searchPlaceholder')} />
       <div className="header-actions">
